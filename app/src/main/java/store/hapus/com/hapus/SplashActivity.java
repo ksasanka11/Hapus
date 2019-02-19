b33d53b1c4305b3a9713d6b718b0ac99d25cdd44
ksasanka11 <30541568+ksasanka11@users.noreply.github.com>
2019-02-19 20:17:38 +0530

    Initial commit

---
+++ app/src/main/java/store/hapus/com/hapus/SplashActivity.java
@@ -0,0 +1,20 @@
+package store.hapus.com.hapus;
+
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        SystemClock.sleep(3000);
+        Intent loginIntent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+}
