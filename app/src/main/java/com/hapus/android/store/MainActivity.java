5c585cfd47814a34d80f63e6bd6a6a3f7d2439a6
ksasanka11 <30541568+ksasanka11@users.noreply.github.com>
2019-02-23 14:13:21 +0530

    Connected to firebase and store. TODO: forgot password

---
+++ app/src/main/java/com/hapus/android/store/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hapus.android.store;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
