39efc68c9ef71dcf5664fd9e191ecc59b0a9460b
ksasanka11 <30541568+ksasanka11@users.noreply.github.com>
2019-04-20 09:32:15 +0530

    Created Banner Slider(15) To-do forgot password

--- app/src/main/java/com/hapus/android/store/HomeFragment.java
+++ app/src/main/java/com/hapus/android/store/HomeFragment.java
@@ -4,18 +4,24 @@ package com.hapus.android.store;
 import android.content.Context;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 /**
@@ -29,6 +35,15 @@ public class HomeFragment extends Fragment {
 
     private RecyclerView categoryRecyclerView;
     private CategoryAdapter categoryAdapter;
+
+    ////////// Banner Slider
+    private ViewPager bannerSliderViewPager;
+    private List<SliderModel> sliderModelList;
+    private int currentPage = 2;
+    private Timer mTimer;
+    final private long DELAY_TIME = 3000;
+    final private long PERIOD_TIME = 3000;
+    ////////// Banner Slider
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,10 +67,108 @@ public class HomeFragment extends Fragment {
         categoryRecyclerView.setAdapter(categoryAdapter);
         categoryAdapter.notifyDataSetChanged();
 
+        ////////// Banner Slider
+
+        bannerSliderViewPager = view.findViewById(R.id.banner_slider_view_pager);
+        sliderModelList = new ArrayList<>();
+
+        sliderModelList.add(new SliderModel(R.drawable.ic_menu_gallery, "#5bc3c7"));
+        sliderModelList.add(new SliderModel(R.drawable.home, "#5bc3c7"));
+        sliderModelList.add(new SliderModel(R.mipmap.app_icon_round, "#5bc3c7"));
+
+        sliderModelList.add(new SliderModel(R.drawable.cart_black, "#5bc3c7"));
+        sliderModelList.add(new SliderModel(R.drawable.close_cross, "#5bc3c7"));
+        sliderModelList.add(new SliderModel(R.drawable.add_profile_pic, "#5bc3c7"));
+        sliderModelList.add(new SliderModel(R.drawable.ic_menu_gallery, "#5bc3c7"));
+
+        sliderModelList.add(new SliderModel(R.drawable.home, "#5bc3c7"));
+        sliderModelList.add(new SliderModel(R.mipmap.app_icon_round, "#5bc3c7"));
+        sliderModelList.add(new SliderModel(R.drawable.cart_black, "#5bc3c7"));
+
+        SliderAdapter sliderAdapter = new SliderAdapter(sliderModelList);
+        bannerSliderViewPager.setAdapter(sliderAdapter);
+        bannerSliderViewPager.setClipToPadding(false);
+        bannerSliderViewPager.setPageMargin(20);
+        bannerSliderViewPager.setCurrentItem(currentPage);
+        ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                currentPage = i;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+                if(i == ViewPager.SCROLL_STATE_IDLE){
+                    pageLooper();
+                }
+            }
+        };
+        bannerSliderViewPager.addOnPageChangeListener(onPageChangeListener);
+
+        startBannerSlideShow();
+
+        bannerSliderViewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                pageLooper();
+                stopBannerSlideShow();
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP);
+                    startBannerSlideShow();
+                return false;
+            }
+        });
+        ////////// Banner Slider
+
         return view;
     }
 
-    public class SpacingItemDecoration extends RecyclerView.ItemDecoration{
+    ////////// Banner Slider
+
+    private void pageLooper(){
+        if(currentPage == sliderModelList.size()-2){
+            currentPage = 2;
+            bannerSliderViewPager.setCurrentItem(currentPage, false);
+        }
+
+        if(currentPage == 1){
+            currentPage = sliderModelList.size()-3;
+            bannerSliderViewPager.setCurrentItem(currentPage, false);
+        }
+    }
+
+    private void startBannerSlideShow(){
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage >= sliderModelList.size())
+                    currentPage = 1;
+                bannerSliderViewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        mTimer = new Timer();
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, DELAY_TIME, PERIOD_TIME);
+    }
+
+    private void stopBannerSlideShow(){
+        mTimer.cancel();
+    }
+
+    ////////// Banner Slider
+
+    /*public class SpacingItemDecoration extends RecyclerView.ItemDecoration{
         private int spacing;
 
         public SpacingItemDecoration(Context context, int padding) {
@@ -70,6 +183,6 @@ public class HomeFragment extends Fragment {
                 outRect.bottom = spacing;
             }
         }
-    }
+    }*/
 
 }
---
+++ app/src/main/java/com/hapus/android/store/SliderAdapter.java
@@ -0,0 +1,49 @@
+package com.hapus.android.store;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+public class SliderAdapter extends PagerAdapter {
+
+    private List<SliderModel> mSliderModelList;
+
+    public SliderAdapter(List<SliderModel> sliderModelList) {
+        mSliderModelList = sliderModelList;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(container.getContext()).inflate(R.layout.slider_layout, container, false);
+        ConstraintLayout bannerContainer = view.findViewById(R.id.banner_container);
+        bannerContainer.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor(mSliderModelList.get(position).getBackgroundColor())));
+        ImageView banner = view.findViewById(R.id.banner_slide);
+        banner.setImageResource(mSliderModelList.get(position).getBanner());
+        container.addView(view, 0);
+        return view;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+
+    @Override
+    public int getCount() {
+        return mSliderModelList.size();
+    }
+}
